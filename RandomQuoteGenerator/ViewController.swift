240f9ae263dec19908516eabf2cb0fd0835e807a
dav-jordan <davisridleyjordan@gmail.com>
2017-12-27 14:22:43 -0600

    First Commit! Woohoo!

---
+++ RandomQuoteGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomQuoteGenerator
+//
+//  Created by Davis Jordan on 12/27/17.
+//  Copyright © 2017 Davis Jordan. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
