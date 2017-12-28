366f42110f256ec9646600a7e5163c41d0b66102
dav-jordan <davisridleyjordan@gmail.com>
2017-12-27 20:11:09 -0600

    Imported DLList and started testing

--- RandomQuoteGenerator/ViewController.swift
+++ RandomQuoteGenerator/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
-
+import RandomQuoteGenerator
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        var list:DLList = DLList()
     }
 
 
-    override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
