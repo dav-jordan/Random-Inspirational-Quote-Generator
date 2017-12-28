2b8885d3d0429ebbf49913f5d1105eafb3868a56
dav-jordan <davisridleyjordan@gmail.com>
2017-12-28 15:18:34 -0600

    Added loadTemplates and started implementing

--- RandomQuoteGenerator/ViewController.swift
+++ RandomQuoteGenerator/ViewController.swift
@@ -34,6 +34,19 @@ class ViewController: UIViewController {
 
     func loadTemplates(list:DLList)
     {
+        //add first template
+        var  n:DLNode? = DLNode(value:"The " + blank1 + " epidemic is caused by " + blank2 + ".")
+        templates?.insertFront(node:n)
+
+        //print for testing
+        DLList.printList(list:templates!)
+
+        //add second template
+        n = nil
+        n = DLNode(value:"The only way to a person's " + blank1 + " is with your " + blank2 + ".")
+
+        //print for testing
+        DLList.printList(list:templates!)
 
     }
 
