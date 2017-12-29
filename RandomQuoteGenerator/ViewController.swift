8f3a1542c17f9403a78899fc7b8ce028e60e2091
dav-jordan <davisridleyjordan@gmail.com>
2017-12-28 22:31:33 -0600

    Took of TODO and changed template in function to list

--- RandomQuoteGenerator/ViewController.swift
+++ RandomQuoteGenerator/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
     {
         //add first template
         var  n:DLNode? = DLNode(value:"The " + blank1 + " epidemic is caused by " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //print for testing
         DLList.printList(list:templates!)
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
         //add second template
         n = nil
         n = DLNode(value:"The only way to a person's " + blank1 + " is with your " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //print for testing
         DLList.printList(list:templates!)
@@ -58,41 +58,42 @@ class ViewController: UIViewController {
         //add third template
         n = nil
         n = DLNode(value:"Always " + blank1 + " your " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add fourth template
         n = nil
         n = DLNode(value:blank1 + " people always " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add fifth template
         n = nil
         n = DLNode(value:"To live is to " + blank1 + " your " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add sixth template
         n = nil
         n = DLNode(value:blank1 + " a " + blank2 + ". You won't regret it.")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add seventh template
         n = nil
         n = DLNode(value:"Don't ponder the " + blank1 + ". You will only find " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add eighth template
         n = nil
         n = DLNode(value:"There are bad ways to " + blank1 + " and good ways to " + blank2 + ", but it isn't always obvious which is which.")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add ninth template
         n = nil
         n = DLNode(value:"Whatever you can " + blank1 + ", you can " + blank2 + ".")
-        templates?.insertFront(node:n)
+        list.insertFront(node:n)
 
         //add tenth template
         n = nil
         n = DLNode(value:"It is in our " + blank1 + " moments where we must focus to see the " + blank2 + ".")
+        list.insertFront(node:n)
 
     }
 
