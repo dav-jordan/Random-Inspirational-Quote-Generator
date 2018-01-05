ba60895390c6df19edba80f2e9f08132f6ce668f
dav-jordan <davisridleyjordan@gmail.com>
2018-01-05 15:02:43 -0600

    Added word array and addWord function

--- RandomQuoteGenerator/WordList.swift
+++ RandomQuoteGenerator/WordList.swift
@@ -8,5 +8,16 @@ import Foundation
 //class that will be used to make a group of words and get a word from that group
 class WordList
 {
+    //array to store words
+    var words:[String]
 
+    //initializer
+    init()
+    {}
+
+    //used to add a word to words array
+    func addWord(add:String)
+    {
+        words.append(add)
+    }
 }
